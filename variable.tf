6183378ef56a284148811aca453b5344495952a8
Tan <ntaniyaa7@gmail.com>
2025-12-07 13:57:20 +0530

    fish

--- variable.tf
+++ variable.tf
@@ -2,13 +2,13 @@ variable "studentapp_instance_type" {
   default = "t3.micro"
 }
 variable "studentapp_ami" {
-  default = "ami-02b8269d5e85954ef"
+  default = "ami-0d2aad4794ad02001"
 }
 variable "studentapp_key_name" {
-  default = "Oct30"
+  default = "nana"
 }
 variable "studentapp_vpc_security_group_ids" {
-  default = "sg-0a1badee4156834ca"
+  default = "sg-05d8d9380629ad735"
 }
 variable "studentapp_disable_api_termination" {
   default = "false"
