188e09504d311f7e30bde56bef6d03637f5fef75
tanniiiyaa <ntaniyaa7@gmail.com>
2025-12-07 08:39:12 +0000

    local changes

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "studentapp" {
-  instance_type = var.studentapp_instance_type
-  ami = var.studentapp_ami
-  key_name = var.studentapp_key_name
-  vpc_security_group_ids = [ var.studentapp_vpc_security_group_ids ]
+  instance_type           = var.studentapp_instance_type
+  ami                     = var.studentapp_ami
+  key_name                = var.studentapp_key_name
+  vpc_security_group_ids  = [var.studentapp_vpc_security_group_ids]
   disable_api_termination = var.studentapp_disable_api_termination
-  user_data = <<-EOT
+  user_data               = <<-EOT
     #!/bin/bash
      sudo apt update -y
 
